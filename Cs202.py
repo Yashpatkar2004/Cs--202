444099e074c9204d1af4ecbe11873843a05e08f6
Yashpatkar2004 <117573410+Yashpatkar2004@users.noreply.github.com>
2025-01-09 10:16:42 +0530

    Update Cs202.py

--- Cs202.py
+++ Cs202.py
@@ -1,7 +1,9 @@
-a=12 '''a is variable 1'''
-b=1  '''b is variable 2 '''
-c = a+b '''c is variable which holds answer'''
-if c ==13: ''' case if ans is right'''
-    print("Answer is 13") '''if it right'''
-else:'''or'''
-    print("wrong")'''wrong'''
+a = 12  # a is variable 1
+b = 1   # b is variable 2
+c = a + b  # c is variable which holds the answer
+
+if c == 13:  # case if answer is right
+    print("Answer is 13")  # if it is right
+else:  # or
+    print("wrong")  # wrong
+
