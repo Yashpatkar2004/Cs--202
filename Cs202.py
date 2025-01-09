13f0d5b107f8a6bdd5a1e2e1ba152ad1d759adf8
Yashpatkar2004 <117573410+Yashpatkar2004@users.noreply.github.com>
2025-01-09 10:21:59 +0530

    Update Cs202.py

--- Cs202.py
+++ Cs202.py
@@ -1,8 +1,7 @@
-a = 12  # a is variable 1
-b = 1   # b is variable 2
-c = a + b  # c is variable which holds the answer
-
-if c == 13:  # case if answer is right
+A = 12  # a is variable 1
+B = 1   # b is variable 2
+C = A + B  # c is variable which holds the answer
+if C == 13:  # case if answer is right
     print("Answer is 13")  # if it is right
 else:  # or
     print("wrong")  # wrong
